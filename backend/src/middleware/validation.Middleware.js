cf8ed5ca25ebfe46764b697e8c87404e5f1dba53
mrnazmul68 <mrnazmul68@gmail.com>
2026-06-11 15:42:42 +0600

    Fixed some issues!

--- backend/src/middleware/validation.Middleware.js
+++ backend/src/middleware/validation.Middleware.js
@@ -1,4 +1,6 @@
 import { ZodError } from "zod";
+import { HTTP_STATUS } from "../shared/constant";
+import { ApiError } from "../utils/ApiError";
 
 export const validate = (schema) => (req, res, next) => {
   try {
@@ -11,17 +13,18 @@ export const validate = (schema) => (req, res, next) => {
   } catch (error) {
     if (error instanceof ZodError) {
       const errors = error.issues.map((err) => ({
-        // ✅ errors → issues
         field: err.path.length ? err.path.join(".") : "unknown",
         message: err.message,
       }));
 
-      res.status(422).json({
-        success: false,
-        errors,
-      });
-    } else {
-      next(error);
-    }
+      return next(
+        new ApiError(
+          HTTP_STATUS.UNPROCESSABLE_ENTITY,
+          "Validation Failed",
+          errors,
+        ),
+      );
+    } 
+    return next(error)
   }
 };
--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,5 +1,11 @@
-export const asyncHandler = (reqHandler) => (req, res, next) => {
-  //reqHandler(req, res, next) may return:a Promise (if async function ✅)or normal value
-  // 👉 Promise.resolve() ensures it's always treated like a Promise.
-  Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
+export const asyncHandler = (reqHandler) => async (req, res, next) => {
+  try {
+    await reqHandler(req, res, next);
+  } catch (error) {
+    next(error)
+  }
 };
+
+// export const asyncHandler = (reqHandler) => (req, res, next) => {
+//   Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
+// };
